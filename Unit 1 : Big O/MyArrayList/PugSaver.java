3449df12d5994a9aac9251b8336bb9c196415a28
GXboy12345 <garretmorbergnguyen@gmail.com>
2026-08-30 19:58:13 -0700

    Commit so approach of streams is present

--- Unit 1 : Big O/MyArrayList/PugSaver.java
+++ Unit 1 : Big O/MyArrayList/PugSaver.java
@@ -1,19 +1,14 @@
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 public class PugSaver {
 
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
-	public static void rescuePugs(MyArrayList<Dog> list) {
-		int l = 0;
-		int r = list.size() - 1;
-		while (l < r) {
-			while (!(list.get(l).getBreed().equals("Pug")) && l < r) l++;
-			while ((list.get(r)).getBreed().equals("Pug") && l < r) r--;
-			if (l < r) {
-				list.swap(l, r);
-				l++;
-				r--;
-			}
-		}
+	public static void rescuePugs(ArrayList<Dog> list) {
+		Collections.copy(list, Stream.concat (
+			list.stream().filter(d -> !d.getBreed().equals("Pug")),
+			list.stream().filter(d -> d.getBreed().equals("Pug"))
+		).toList());
 	}
 }
